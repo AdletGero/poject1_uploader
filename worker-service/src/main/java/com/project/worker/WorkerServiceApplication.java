c09a0718c05d3f3338f04077d934217d68801483
Adlett Anafiya <adlet.anafiya@mail.ru>
2026-02-03 19:54:58 +0500

    commit3

--- worker-service/src/main/java/com/project/worker/WorkerServiceApplication.java
+++ worker-service/src/main/java/com/project/worker/WorkerServiceApplication.java
@@ -2,7 +2,9 @@ package com.project.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class WorkerServiceApplication {
     public static void main(String[] args) {
